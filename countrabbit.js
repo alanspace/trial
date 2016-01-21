02e638c2254c8ab2505412cb6cde50b04966883b
alanspace <mail.alanleung@gmail.com>
2016-01-21 11:00:41 +0800

    Create countrabbit.js

---
+++ countrabbit.js
@@ -0,0 +1,13 @@
+	    <script>
+
+	      function count_rabbits() {
+
+	        for(var i=1; i<=3; i++) {
+
+	          alert("Rabbit "+i+" out of the hat!");
+
+	        }
+
+	      }
+
+	    </script>
